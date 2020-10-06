fe10e445083229fcb7334e3b49a333b80757b12a
icemtel <icemtel@gmail.com>
2020-10-06 13:00:35 +0200

    add vedo_viewer and examples

--- mesh/plot/vedo_viewer.py
+++ mesh/plot/vedo_viewer.py
@@ -1,10 +1,14 @@
 """
-Visualzie using `trimesh` package
+Visualzie using `vedo` (a.k.a. `vtkplotter`?) package
 
-- I don't define my own MeshViewer class, because trimesh.Trimesh class is already very functional
+- I don't define my own MeshViewer class, because vedo.Mesh class is already very functional
+
+# In jupyter:
+# vedo.embedWindow(False)
+#embedWindow('2d') # for a static image
 """
 
-import trimesh
+import vedo
 import FBEM
 import numpy as np
 
@@ -19,14 +23,29 @@ def get_colors(num, cmap, endpoint=True):
     cm = getattr(mcm, cmap)
     return cm(np.linspace(0, 1, num, endpoint=endpoint))
 
-def triangulation_to_trimesh(tri):
+def get_cilium_colors_rgb(num_phases, cmap='hsv'):
+    '''
+    Returns colors, coding cilia phases.
+    '''
+    colors = get_colors(num_phases, cmap, endpoint=False)
+    colors = [c[:3] for c in colors] # only RGB, no alpha
+    return colors
+
+
+def triangulation_to_vedo_mesh(tri, **kwargs):
     """
     Transform my triangulation class to Trimesh class
+    :param kwargs:
     """
     coords = tri.coordinates
     trias = tri.triangulation
-    m = trimesh.Trimesh(vertices=coords, faces=trias,
-                        validate=True)
+    m = vedo.Mesh([coords, trias], **kwargs)
+    return m
+
+
+def source_to_vedo_mesh(source, name, **kwargs):
+    coords, trias = source.read_triangulation(name)
+    m = vedo.Mesh([coords, trias], **kwargs)
     return m
 
 
@@ -36,45 +55,48 @@ def MeshViewer(path, names=None, colors=None, group='.'):
     - set colors to each object
     :param path:
     :param group:
-    :param names: list of names of objects to plot
+    :param names: list of names of objects to plot; or a single name
     :param colors: list of colors - 4-tuples;
                    if not given- use random colors; some entries are None => replace with random colors
     :return:
     """
     source = FBEM.Source(path, group)
+    # if names is a single name string
+    if isinstance(names, str):
+        names = [names]
     # if no names are given - get all names from ranges
-    if names is None:  #
+    elif names is None:  #
         names = source.read_ranges().get_names()
     # if no colors are given - define a list of matching length
     if colors is None:
         colors = [None for _ in names]
-    # replace None's with random colors
-    for icolor, color in enumerate(colors):
-        if color is None:
-            random_color = trimesh.visual.random_color()
-            colors[icolor] = random_color
 
-    nodes_list, trias_list = source.read_triangulation_list(names)
+    for i, c in enumerate(colors):
+        if c is None:
+            color = np.random.rand(3)
+            colors[i] = color
 
     meshes = []
-    for nodes, trias in zip(nodes_list, trias_list):
-        m = trimesh.Trimesh(vertices=nodes, faces=trias, validate=True)
+    for color, name in zip(colors, names):
+        m = source_to_vedo_mesh(source, name, c=color)
         meshes.append(m)
 
-    for m, c in zip(meshes, colors):
-        m.visual.face_colors = c
 
-    m_combined = sum(meshes)
+#   m_combined = vedo.merge(*meshes) # loses individual colors
+    m_combined = meshes[0]
+    for m in meshes[1:]:
+        m_combined += m
     return m_combined
 
 
-def FlagellaPlaneViewer(path, phases, num_phases, group='.', cmap='hsv', plane_color=(0.9, 0.9, 0.9, 1)):
+def FlagellaPlaneViewer(path, phases, num_phases, group='.', cmap='hsv', plane_color='lightgrey'):
     '''
     Visualize flagella and the plane.
     - Color flagella in accordance with their phase
     - Assume first N objects - cilia/flagella, and N+1th object - plane
     '''
-    colors_list = get_colors(num_phases, cmap=cmap, endpoint=False)
+    colors_list = get_cilium_colors_rgb(num_phases, cmap=cmap)
+    # Colors to RGB
 
     flagella_colors = []
     for phase in phases:
