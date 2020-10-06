7165bec5b553755cf7fbded9a441533bc44cd8fc
icemtel <icemtel@gmail.com>
2020-10-06 15:54:18 +0200

    upd example name

---
+++ mesh/plot/example/vedo_viewer_cilia.py
@@ -0,0 +1,11 @@
+import mesh
+import mesh.plot.vedo_viewer as mplot
+
+folder = 'data/cilia_plane2'
+
+num_phases = 20
+phases = [0, 5]
+
+mv = mplot.FlagellaPlaneViewer(folder, phases=phases, num_phases=num_phases)
+plotter = mv.show()
+plotter.close()
