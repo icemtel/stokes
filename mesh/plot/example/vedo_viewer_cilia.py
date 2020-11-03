719a46c32d81a8d720965a6dce9e2ee32174ffff
icemtel <icemtel@gmail.com>
2020-11-03 12:03:33 +0100

    upd: vedo example upd: comment in parallel with threads

--- mesh/plot/example/vedo_viewer_cilia.py
+++ mesh/plot/example/vedo_viewer_cilia.py
@@ -1,13 +1,18 @@
 import mesh
 import mesh.plot.vedo_viewer as mplot
 
-folder = 'data/cilia_plane2'
+folder = 'data/two_cilia_machemer0/' # cilia_plane'
 
-num_phases = 20
-phases = [0, 5]
+mv = mplot.MeshViewer(folder)
+# plotter = mv.show()
+# plotter.close()
 
-mv = mplot.FlagellaPlaneViewer(folder, phases=phases, num_phases=num_phases)
-plotter = mv.show()
+# Plot with arrows
+names = ['flagellum_1', 'flagellum_2']
+scale = 1
+min_arrow_length = 0  # 1 um
+arrows = mplot.VelocityArrows(folder, names, points_per_arrow=6, scale=scale, min_arrow_length=min_arrow_length)
+
+assembly = mv + arrows
+plotter = assembly.show()
 plotter.close()
-
-
